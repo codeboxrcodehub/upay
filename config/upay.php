b4deca9c7e5b44db890510d196e45392c84a968e
Dipu Chandra Dey <dipudey764@gmail.com>
2022-09-01 16:38:56 +0600

    update config

--- config/upay.php
+++ config/upay.php
@@ -9,6 +9,6 @@ return [
     "merchant_name"          => env("UPAY_MERCHANT_NAME", ""),
     "merchant_mobile"        => env("UPAY_MERCHANT_MOBILE", ""),
     "merchant_city"          => env("UPAY_MERCHANT_CITY", ""),
-    "merchant_category_code" => env("UPAY_CATEGORY_CODE", "9399"),
+    "merchant_category_code" => env("UPAY_CATEGORY_CODE", ""),
     "redirect_url"           => env("UPAY_CALLBACK_URL", "")
 ];
